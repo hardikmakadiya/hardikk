9e01d057ae7802633b9633804f240b1e8900eb71
hardikmakadiya <hardikmakadiya12@gmail.com>
2024-12-15 20:36:03 +0530

    Create contact_me.php
    contact_me.php

---
+++ contact_me.php
@@ -0,0 +1,74 @@
+<?php
+// Check for empty fields
+// if(empty($_POST['name'])      ||
+//    empty($_POST['email'])     ||
+//    empty($_POST['phone'])     ||
+//    empty($_POST['message'])   ||
+//    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+//    {
+//    echo "No arguments Provided!";
+//    return false;
+//    }
+   
+// $name = strip_tags(htmlspecialchars($_POST['name']));
+// $email_address = strip_tags(htmlspecialchars($_POST['email']));
+// $phone = strip_tags(htmlspecialchars($_POST['phone']));
+// $message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// // Create the email and send the message
+// $to = 'hardikmakdiya12@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+// $email_subject = "Website Contact Form:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";   
+// mail($to,$email_subject,$email_body,$headers);
+// return true; 
+
+
+// Replace this with your own email address
+$to = 'hardikmakadiya12@gmail.com';
+
+function url(){
+  return sprintf(
+    "%s://%s",
+    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+    $_SERVER['SERVER_NAME']
+  );
+}
+
+if($_POST) {
+
+   $name = trim(stripslashes($_POST['name']));
+   $email = trim(stripslashes($_POST['email']));
+   $subject = trim(stripslashes($_POST['subject']));
+   $contact_message = trim(stripslashes($_POST['message']));
+
+   
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+   // Set Message
+   $message .= "Email from: " . $name . "<br />";
+	 $message .= "Email address: " . $email . "<br />";
+   $message .= "Message: <br />";
+   $message .= nl2br($contact_message);
+   $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <" . $email . ">";
+
+   // Email Headers
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+   ini_set("sendmail_from", $to); // for windows server
+   $mail = mail($to, $subject, $message, $headers);
+
+	if ($mail) { echo "OK"; }
+   else { echo "Something went wrong. Please try again."; }
+
+}
+
+        
+?>
